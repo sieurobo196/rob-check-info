42e226cd647ab0b8feb6597962732a7ea592769e
Sieurobo196 <32062493+sieurobo196@users.noreply.github.com>
2023-07-30 21:10:02 +0700

    Create MatchData.java

---
+++ src/main/java/com/cwr/model/MatchData.java
@@ -0,0 +1,15 @@
+package com.cwr.model;
+
+import java.util.List;
+
+public class MatchData {
+    private List<List<Object>> data;
+
+    public List<List<Object>> getData() {
+        return data;
+    }
+
+    public void setData(List<List<Object>> data) {
+        this.data = data;
+    }
+}
